f183734f9df4c4eb4935ec9af871330632416499
TOMA <thomasdarcheville1601@gmail.com>
2024-10-18 14:05:18 +0200

    :sparkles: Presentation page created.

--- src/app/ui/navbar/ThomasLogo.tsx
+++ src/app/ui/navbar/ThomasLogo.tsx
@@ -1,7 +1,7 @@
 const ThomasLogo = () => {
 
     return (
-        <a href="#presentation" className='text-3xl font-semibold'>
+        <a href="/#presentation" className='text-3xl font-semibold'>
             <span className='text-primary text-4xl'>T</span>homas.
         </a>
     )
